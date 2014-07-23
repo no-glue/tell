af49e9fe3e783c5d16b5e89fc21aaa3344be5d0a
no-glue <nikolapav1985@gmail.com>
2014-07-23 14:43:49 +0200

    tell from

--- tell.erl
+++ tell.erl
@@ -1,8 +1,11 @@
 -module(tell).
--export([tell/2, reply/2]).
+-export([tell/2, tell_from/3, reply/2]).
 
 tell(Pid, Message)->Pid!{request, self(), Message}, receive {reply, Reply}->Reply end.
 % tells something to a receiving process
+
+tell_from(Pid, Message, From)->Pid!{request, From, Message}, receive {reply, Reply}->Reply end.
+% tell from some process
 
 reply(Pid, Message)->Pid!{reply, Message}.
 % replies to a process
